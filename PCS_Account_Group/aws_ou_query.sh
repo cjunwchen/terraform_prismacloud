60f3d66c7488e82aa91e5d863fa9952cb64c523d
Chen Jun <39447592+cjunwchen@users.noreply.github.com>
2023-08-07 17:08:32 +0700

    Update aws_ou_query.sh

--- PCS_Account_Group/aws_ou_query.sh
+++ PCS_Account_Group/aws_ou_query.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 ACC_ID=$1
-#ACC_ID="032273940486"
-
-#OU_BLOB=$(aws organizations list-accounts | jq ".Accounts[] | select(.Id ==\"$ACC_ID\") 
-#OU_ID=$(jq -r '.Arn' <<< $OU_BLOB | awk -F "/" '{print $2}')
-#ACC_NAME=$(jq -r '.Name' <<< $OU_BLOB )
 
 ACC_NAME=$(aws organizations list-accounts | jq ".Accounts[] | select(.Id ==\"$ACC_ID\") | .Name" | tr -d \") 
 OU_Parent_ID=$(aws organizations list-parents --child-id $ACC_ID | jq -r '.Parents[] | .Id')
